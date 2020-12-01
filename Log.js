0232f23ec09f8aa254c3674611c6ea2675e4b6b0
JatinReddy <73945541+JatinReddy@users.noreply.github.com>
2020-12-01 20:03:13 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,23 @@
+class Log {
+    constructor(x,y,width,height) {
+        var options = {
+            isStatic : false,
+            restitution : 0 
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        
+    }
+    
+        
+        display(){
+            fill("red");
+            var pos = this.body.position;
+            rectMode (CENTER);
+             
+            rect(pos.x,pos.y,this.width,this.height);
+            
+        }
+    
+    }
